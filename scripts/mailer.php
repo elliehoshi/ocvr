e06c4a4f385c83c8c248d63be2216ac9e3de259b
Kendrick Parks <bordersmbk02@aol.com>
2014-05-19 14:26:20 -0700

    trying to add mailer

---
+++ scripts/mailer.php
@@ -0,0 +1,23 @@
+<?php
+
+function mailer() {
+  $name = $_POST["name"];
+  $body = $_POST["body"];
+  $phone = $_POST["phone"];
+  $email = $_POST["email"];
+
+  $to      = 'parks.kendrick@gmail.com';
+  $subject = "Sponsor Request from " . $name ;
+  $message = "Sponsor Message: \r" . $body . "\rPhone Number: " . $phone;
+  $header = "From: info@smogfightingpaint.com\r\n"; 
+  $header.= "MIME-Version: 1.0\r\n"; 
+  $header.= "Content-Type: text/plain; charset=utf-8\r\n"; 
+  $header.= "X-Priority: 1\r\n"; 
+  $header.= 'X-Mailer: PHP/' . phpversion();
+  $header.= "Reply-To: " . $email . "\r\n";
+
+  mail($to, $subject, $message, $header);
+}
+
+
+?>
