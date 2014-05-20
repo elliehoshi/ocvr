b5c3e437b72f002df557be7b1b40f2904027df5f
Kendrick Parks <bordersmbk02@aol.com>
2014-05-19 21:01:03 -0700

    form mailer working

--- scripts/mailer.php
+++ scripts/mailer.php
@@ -1,6 +1,5 @@
 <?php
 
-function mailer() {
   $name = $_POST["name"];
   $body = $_POST["body"];
   $phone = $_POST["phone"];
@@ -9,16 +8,20 @@ function mailer() {
   $to      = 'parks.kendrick@gmail.com';
   $subject = "Sponsor Request from " . $name ;
   $message = "Sponsor Message: \r" . $body . "\rPhone Number: " . $phone;
-  $header = "To: Mary <mary@example.com>\r\n";
-  $header.= "From: info@smogfightingpaint.com\r\n"; 
+  $header = "To: Meesh <michel@orangecountyvr.com>, Dylan <dylan@orangecountyvr.com>\r\n";
+  $header.= "From: SponsorPage <info@smogfightingpaint.com>\r\n"; 
   $header.= "MIME-Version: 1.0\r\n"; 
   $header.= "Content-Type: text/plain; charset=utf-8\r\n"; 
   $header.= "X-Priority: 1\r\n"; 
   $header.= 'X-Mailer: PHP/' . phpversion();
   $header.= "Reply-To: " . $email . "\r\n";
 
-  mail($to, $subject, $message, $header, "-finfo@smogfightingpaint.com");
-}
+  mail($to, $subject, $message, $header);
+  // mail($to, $subject, $message, "From: Info <info@smogfightingpaint.com>");
+  header( "Location: http://smogfightingpaint.com/kendrick/sponsors/index.html" );
+  // die('here');
+  exit();
+
 
 
 ?>
